6e5354e9d7516fc72607725c8dfb5bf432136d09
Chris Buchert <chris@smartystreets.com>
2019-03-01 11:24:46 -0700

    Stubbed out app.

---
+++ src/Autocomplete.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default class Autocomplete extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+
+		};
+	}
+
+	render() {
+		return (
+			<div>
+				Sanity test.
+			</div>
+		);
+	}
+}
