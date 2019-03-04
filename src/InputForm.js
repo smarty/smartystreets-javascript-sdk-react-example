0839940745804585e950ee0fff6335128380f223
Chris Buchert <chris@smartystreets.com>
2019-03-04 10:39:56 -0700

    Form is styled with flexbox.

--- src/InputForm.js
+++ src/InputForm.js
@@ -1,53 +1,102 @@
 import React from "react";
+import "./InputForm.scss";
 
 export default function InputForm({address1, address2, locality, province, postalCode, country, updateField, queryAutocompleteForSuggestions}) {
 	return (
-		<form>
-			<label htmlFor="address1">Address 1</label>
-			<input
-				type="text"
-				name={"address1"}
-				value={address1}
-				onChange={e => {
-					updateField(e);
-					queryAutocompleteForSuggestions(e.target.value);
-				}}
-			/>
-			<label htmlFor="address2">Address 2</label>
-			<input
-				type="text"
-				name={"address2"}
-				value={address2}
-				onChange={updateField}
-			/>
-			<label htmlFor="locality">City</label>
-			<input
-				type="text"
-				name={"locality"}
-				value={locality}
-				onChange={updateField}
-			/>
-			<label htmlFor="province">Province</label>
-			<input
-				type="text"
-				name={"province"}
-				value={province}
-				onChange={updateField}
-			/>
-			<label htmlFor="postalCode">Postal Code</label>
-			<input
-				type="text"
-				name={"postalCode"}
-				value={postalCode}
-				onChange={updateField}
-			/>
-			<label htmlFor="country">Country</label>
-			<input
-				type="text"
-				name={"country"}
-				value={country}
-				onChange={updateField}
-			/>
+		<form className={"autocomplete--input-form"}>
+			<div className={"autocomplete--input-group"}>
+				<label
+					className={"autocomplete--input-label"}
+					htmlFor="address1"
+				>
+					Address 1
+				</label>
+				<input
+					className={"autocomplete--input-field"}
+					type="text"
+					name={"address1"}
+					value={address1}
+					onChange={e => {
+						updateField(e);
+						queryAutocompleteForSuggestions(e.target.value);
+					}}
+				/>
+			</div>
+			<div className={"autocomplete--input-group"}>
+				<label
+					className={"autocomplete--input-label"}
+					htmlFor="address2"
+				>
+					Address 2
+				</label>
+				<input
+					className={"autocomplete--input-field"}
+					type="text"
+					name={"address2"}
+					value={address2}
+					onChange={updateField}
+				/>
+			</div>
+			<div className={"autocomplete--input-group"}>
+				<label
+					className={"autocomplete--input-label"}
+					htmlFor="locality"
+				>
+					City
+				</label>
+				<input
+					className={"autocomplete--input-field"}
+					type="text"
+					name={"locality"}
+					value={locality}
+					onChange={updateField}
+				/>
+			</div>
+			<div className={"autocomplete--input-group"}>
+				<label
+					className={"autocomplete--input-label"}
+					htmlFor="province"
+				>
+					Province
+				</label>
+				<input
+					className={"autocomplete--input-field"}
+					type="text"
+					name={"province"}
+					value={province}
+					onChange={updateField}
+				/>
+			</div>
+			<div className={"autocomplete--input-group"}>
+				<label
+					className={"autocomplete--input-label"}
+					htmlFor="postalCode"
+				>
+					Postal Code
+				</label>
+				<input
+					className={"autocomplete--input-field"}
+					type="text"
+					name={"postalCode"}
+					value={postalCode}
+					onChange={updateField}
+				/>
+			</div>
+			<div className={"autocomplete--input-group"}>
+				<label
+					className={"autocomplete--input-label"}
+					htmlFor="country"
+				>
+					Country
+				</label>
+				<input
+					className={"autocomplete--input-field"}
+					type="text"
+					name={"country"}
+					value={country}
+					onChange={updateField}
+				/>
+			</div>
 		</form>
 	);
 }
