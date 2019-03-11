2c478b40d924557fae09bdbfb1b5179ea22e7ee3
Chris Buchert <chris@smartystreets.com>
2019-03-11 14:48:16 -0600

    Input fields are created dynamically from a list.

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -3,87 +3,31 @@ import "./InputForm.scss";
 import inputFields from "../data/input_fields";
 import {countries} from "../data/countries";
 
-export default function InputForm({address1, address2, locality, province, postalCode, country, updateField, queryAutocompleteForSuggestions}) {
+export default function InputForm({state, updateField, queryAutocompleteForSuggestions}) {
 	return (
 		<form className={"autocomplete--input-form"}>
-			<div className={"autocomplete--input-group"}>
-				<label
-					className={"autocomplete--input-label"}
-					htmlFor="address1"
-				>
-					Address 1
-				</label>
-				<input
-					className={"autocomplete--input-field"}
-					type="text"
-					id={"address1"}
-					value={address1}
-					onChange={e => {
-						updateField(e);
-						queryAutocompleteForSuggestions(e.target.value);
-					}}
-				/>
-			</div>
-			<div className={"autocomplete--input-group"}>
-				<label
-					className={"autocomplete--input-label"}
-					htmlFor="address2"
-				>
-					Address 2
-				</label>
-				<input
-					className={"autocomplete--input-field"}
-					type="text"
-					id={"address2"}
-					value={address2}
-					onChange={updateField}
-				/>
-			</div>
-			<div className={"autocomplete--input-group"}>
-				<label
-					className={"autocomplete--input-label"}
-					htmlFor="locality"
-				>
-					City
-				</label>
-				<input
-					className={"autocomplete--input-field"}
-					type="text"
-					id={"locality"}
-					value={locality}
-					onChange={updateField}
-				/>
-			</div>
-			<div className={"autocomplete--input-group"}>
-				<label
-					className={"autocomplete--input-label"}
-					htmlFor="province"
-				>
-					Province
-				</label>
-				<input
-					className={"autocomplete--input-field"}
-					type="text"
-					id={"province"}
-					value={province}
-					onChange={updateField}
-				/>
-			</div>
-			<div className={"autocomplete--input-group"}>
-				<label
-					className={"autocomplete--input-label"}
-					htmlFor="postalCode"
-				>
-					Postal Code
-				</label>
-				<input
-					className={"autocomplete--input-field"}
-					type="text"
-					id={"postalCode"}
-					value={postalCode}
-					onChange={updateField}
-				/>
-			</div>
+			{inputFields.map(inputField => {
+				return (
+					<div className={"autocomplete--input-group"} key={inputField.fieldName}>
+						<label
+							className={"autocomplete--input-label"}
+							htmlFor={inputField.fieldName}
+						>
+							{inputField.fieldLabel}
+						</label>
+						<input
+							className={"autocomplete--input-field"}
+							type="text"
+							id={inputField.fieldName}
+							value={state[inputField.fieldName]}
+							onChange={e => {
+								updateField(e);
+								queryAutocompleteForSuggestions(e.target.value);
+							}}
+						/>
+					</div>
+				);
+			})}
 			<div className={"autocomplete--input-group"}>
 				<label
 					className={"autocomplete--input-label"}
---
+++ src/data/input_fields.js
@@ -0,0 +1,24 @@
+const inputFields = [
+	{
+		fieldName: "address1",
+		fieldLabel: "Address 1",
+	},
+	{
+		fieldName: "address2",
+		fieldLabel: "Address 2",
+	},
+	{
+		fieldName: "locality",
+		fieldLabel: "City",
+	},
+	{
+		fieldName: "province",
+		fieldLabel: "State",
+	},
+	{
+		fieldName: "postalCode",
+		fieldLabel: "Zip Code",
+	},
+];
+
+export default inputFields;
